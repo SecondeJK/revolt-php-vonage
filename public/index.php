75d6c583810eed0f37466bab6741a676a1c4d466
James Seconde <jim.seconde@googlemail.com>
2021-11-04 13:52:48 +0000

    outbound calls with mocked data loop

--- public/index.php
+++ public/index.php
@@ -3,24 +3,41 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Vonage\Client;
+use Vonage\Client\Credentials\Keypair;
+use Vonage\Voice\Endpoint\Phone;
+use Vonage\Voice\OutboundCall;
+use Vonage\Voice\Webhook;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+function getmicrotime($t): float
+{
+    list($usec, $sec) = explode(" ", $t);
+
+    return ((float)$usec + (float)$sec);
+}
+
 $app = AppFactory::create();
 
-$keypair = new \Vonage\Client\Credentials\Keypair(
+$keypair = new Keypair(
     file_get_contents('../revolt_php_example.key'),
     '940597b9-7f52-416f-8fd4-a19e0f689602'
 );
 
 $vonage = new Client($keypair);
 
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('RevoltPHP + Vonage Example App');
+
+    return $response;
+});
+
 $app->get('/webhook/answer', function (Request $request, Response $response) {
     $ncco = [
         [
             'action' => 'talk',
             'language' => 'en-GB',
-            'style' => 7,
+            'style' => 6,
             'text' => 'Emergency on factory floor, evacuate to exits immediately'
         ]
     ];
@@ -31,22 +48,43 @@ $app->get('/webhook/answer', function (Request $request, Response $response) {
         ->withHeader('Content-Type', 'application/json');
 });
 
-$app->get('/makeCall/{number}', function (Request $request, Response $response, array $args) use ($vonage) {
-    $client->calls()->create([
-        'to' => [[
-            'type' => 'phone',
-            'number' => $args['number']
-        ]],
-        'from' => [
-            'type' => 'phone',
-            'number' => 'YOUR_VONAGE_NUMBER'
-        ],
-        'answer_url' => ['https://afb8ad306a73.ngrok.io/webhook/answer']
-    ]);
+$app->get('/emergencyNotification', function (Request $request, Response $response) use ($vonage) {
+    $startTime = microtime();
 
-    return $response
-        ->withHeader('Content-Type', 'application/json')
-        ->withStatus(200);
+    $faker = Faker\Factory::create('en_GB');
+    $phoneNumbers = [];
+
+    for ($i = 0; $i < 4001; $i++) {
+        sleep(1);
+        $phoneNumbers[] = $faker->phoneNumber();
+    }
+
+    foreach ($phoneNumbers as $outboundNumber) {
+        $outboundCall = new OutboundCall(
+            new Phone($outboundNumber),
+            new Phone('+447451284518')
+        );
+
+        $outboundCall
+            ->setAnswerWebhook(
+                new Webhook('https://aef9-82-30-208-179.ngrok.io/webhook/answer', 'GET')
+            )
+            ->setEventWebhook(
+                new Webhook('https://aef9-82-30-208-179.ngrok.io/webhook/event', 'GET')
+            )
+        ;
+
+    //    $outboundResponse = $vonage->voice()->createOutboundCall($outboundCall);
+    }
+
+
+    $response->getBody()->write('Outbound calls created.' . PHP_EOL);
+    $endTime = microtime();
+
+    $time = (getmicrotime($endTime) - getmicrotime($startTime));
+    $response->getBody()->write('Completed function in: ' . $time);
+
+    return $response;
 });
 
 $app->run();
